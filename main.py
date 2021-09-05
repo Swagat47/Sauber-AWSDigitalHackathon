497f69d0fec3c7e81baf9d82bb449ee1a08d2480
baamaboi <samarjiitsingh@gmail.com>
2021-09-05 20:21:51 +0530

    Deleted stray files. Some final touches

--- main.py
+++ main.py
@@ -1,171 +1,57 @@
-#!/usr/bin/env python
+import board, adafruit_dht, os, requests, time
 
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D18)
+send_URL = 'https://api.thingspeak.com/update?'
+send_key = os.environ['SENDKEY']
 
-from awscrt import io, mqtt, auth, http
-from awsiot import mqtt_connection_builder
-import sys
-import threading
-import time
-import os
-from uuid import uuid4
-import json
-from sense import sensor_data
+def sensor_data():
+    data = [None, None, None]
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        data = [temperature_c, temperature_f, humidity]
 
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        return data
 
-#Constants
+    except Exception as error:
+        dhtDevice.exit()
+        return data
+        raise error
+        
 
-endpoint = os.environ['ENDPOINT']
-rootCAPath = os.environ['ROOTCA']
-certificatePath = os.environ['CERT']
-privateKeyPath = os.environ['KEY']
-port = False
-useWebsocket = False
-clientId = "test-" + str(uuid4())
-proxy_options = None
-signing_region = 'us-east-1'
-verbosity = io.LogLevel.Debug.name
-count = 0
-topic = "test/topic"
-message = True
+    return data
 
-# Using globals to simplify sample code
+def genrate_url(key, data, url):
+    final_url = url + 'api_key=' + key
+    count = 1
+    for field in data:
+        final_url += '&field' + str(count) + '=' + str(field)
+        count += 1
+    return final_url
 
-io.init_logging(getattr(io.LogLevel, verbosity), 'stderr')
-
-received_count = 0
-received_all_event = threading.Event()
-
-
-# Port defaults
-if useWebsocket and not port:  # When no port override for WebSocket, default to 443
-    port = 443
-if not useWebsocket and not port:  # When no port override for non-WebSocket, default to 8883
-    port = 8883
-
-# Callback when connection is accidentally lost.
-def on_connection_interrupted(connection, error, **kwargs):
-    print("Connection interrupted. error: {}".format(error))
-
-
-# Callback when an interrupted connection is re-established.
-def on_connection_resumed(connection, return_code, session_present, **kwargs):
-    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
-
-    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
-        print("Session did not persist. Resubscribing to existing topics...")
-        resubscribe_future, _ = connection.resubscribe_existing_topics()
-
-        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
-        # evaluate result with a callback instead.
-        resubscribe_future.add_done_callback(on_resubscribe_complete)
-
-
-def on_resubscribe_complete(resubscribe_future):
-        resubscribe_results = resubscribe_future.result()
-        print("Resubscribe results: {}".format(resubscribe_results))
-
-        for topic, qos in resubscribe_results['topics']:
-            if qos is None:
-                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
-
-
-# Callback when the subscribed topic receives a message
-def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-    print("Received message from topic '{}': {}".format(topic, payload))
-    global received_count
-    received_count += 1
-    if received_count == count:
-        received_all_event.set()
 
 if __name__ == '__main__':
-    # Spin up resources
-    event_loop_group = io.EventLoopGroup(1)
-    host_resolver = io.DefaultHostResolver(event_loop_group)
-    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    try:
+        while True:
+            data = sensor_data()
+            print(f'Data from Sensor: temp_c = {data[0]}, temp_f = {data[1]}, humidity = {data[2]}')
+            url = genrate_url(send_key, data, send_URL)
+            connection = requests.get(url)
+            print('Attempting to send Data')
+            status_code = connection.status_code
+            print(f'Response Code : {status_code} {responses[status_code]}')
+            connection.close()
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print('Closed by User')
+    except Exception as e:
+        print(e)
 
 
-    if useWebsocket == True:
-        credentials_provider = auth.AwsCredentialsProvider.new_default_chain(client_bootstrap)
-        mqtt_connection = mqtt_connection_builder.websockets_with_default_aws_signing(
-            endpoint=endpoint,
-            client_bootstrap=client_bootstrap,
-            region=signing_region,
-            credentials_provider=credentials_provider,
-            http_proxy_options=proxy_options,
-            ca_filepath=rootCAPath,
-            on_connection_interrupted=on_connection_interrupted,
-            on_connection_resumed=on_connection_resumed,
-            client_id=clientId,
-            clean_session=False,
-            keep_alive_secs=30)
-
-    else:
-        mqtt_connection = mqtt_connection_builder.mtls_from_path(
-            endpoint=endpoint,
-            port=port,
-            cert_filepath=certificatePath,
-            pri_key_filepath=privateKeyPath,
-            client_bootstrap=client_bootstrap,
-            ca_filepath=rootCAPath,
-            on_connection_interrupted=on_connection_interrupted,
-            on_connection_resumed=on_connection_resumed,
-            client_id=clientId,
-            clean_session=False,
-            keep_alive_secs=30,
-            http_proxy_options=proxy_options)
-
-    print("Connecting to {} with client ID '{}'...".format(
-        endpoint, clientId))
-
-    connect_future = mqtt_connection.connect()
-
-    # Future.result() waits until a result is available
-    connect_future.result()
-    print("Connected!")
-
-    # Subscribe
-    print("Subscribing to topic '{}'...".format(topic))
-    subscribe_future, packet_id = mqtt_connection.subscribe(
-        topic=topic,
-        qos=mqtt.QoS.AT_LEAST_ONCE,
-        callback=on_message_received)
-
-    subscribe_result = subscribe_future.result()
-    print("Subscribed with {}".format(str(subscribe_result['qos'])))
-
-    # Publish message to server desired number of times.
-    # This step is skipped if message is blank.
-    # This step loops forever if count was set to 0.
-    if message:
-        if count == 0:
-            print ("Sending messages until program killed")
-        else:
-            print ("Sending {} message(s)".format(count))
-
-        publish_count = 1
-        while (publish_count <= count) or (count == 0):
-            c, f, h = sensor_data()
-            message = c
-            message = "{} [{}]".format(message, publish_count)
-            print("Publishing message to topic '{}': {}".format(topic, message))
-            message_json = json.dumps(message)
-            mqtt_connection.publish(
-                topic=topic,
-                payload=message_json,
-                qos=mqtt.QoS.AT_LEAST_ONCE)
-            time.sleep(2)
-            publish_count += 1
-
-    # Wait for all messages to be received.
-    # This waits forever if count was set to 0.
-    if count != 0 and not received_all_event.is_set():
-        print("Waiting for all messages to be received...")
-
-    received_all_event.wait()
-    print("{} message(s) received.".format(received_count))
-
-    # Disconnect
-    print("Disconnecting...")
-    disconnect_future = mqtt_connection.disconnect()
-    disconnect_future.result()
-    print("Disconnected!")
+    
