baf040742b79cee92dbe0ef6aeb724143783f0f6
baamaboi <samarjiitsingh@gmail.com>
2021-09-05 14:33:01 +0530

    chnaged main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import logging
+#!/usr/bin/env python
+
+
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import sys
+import threading
 import time
 import os
+from uuid import uuid4
+import json
 from sense import sensor_data
 
-host = os.environ['ENDPOINT']
+
+#Constants
+
+endpoint = os.environ['ENDPOINT']
 rootCAPath = os.environ['ROOTCA']
 certificatePath = os.environ['CERT']
 privateKeyPath = os.environ['KEY']
 port = False
-useWebsocket = True
-clientId = 'sensor'
+useWebsocket = False
+clientId = "test-" + str(uuid4())
+proxy_options = None
+signing_region = 'us-east-1'
+verbosity = io.LogLevel.Debug.name
+count = 0
+tpoic = "test/topic"
+message = True
+
+# Using globals to simplify sample code
+
+io.init_logging(getattr(io.LogLevel, verbosity), 'stderr')
+
+received_count = 0
+received_all_event = threading.Event()
 
 
 # Port defaults
@@ -19,42 +42,130 @@ if useWebsocket and not port:  # When no port override for WebSocket, default to
 if not useWebsocket and not port:  # When no port override for non-WebSocket, default to 8883
     port = 8883
 
-# Configure logging
-logger = logging.getLogger("AWSIoTPythonSDK.core")
-logger.setLevel(logging.DEBUG)
-streamHandler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-streamHandler.setFormatter(formatter)
-logger.addHandler(streamHandler)
-
-# Init AWSIoTMQTTClient
-myAWSIoTMQTTClient = None
-if useWebsocket:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
-    myAWSIoTMQTTClient.configureEndpoint(host, port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
-else:
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(host, port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-
-# AWSIoTMQTTClient connection configuration
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
 
 
-# Publish to the same topic in a loop forever
-while True:
-    c, f, humi = sensor_data()
-    res = []
-    res.append(myAWSIoTMQTTClient.publish("/get", c, 1))
-    res.append(myAWSIoTMQTTClient.publish("/get", f, 1))
-    res.append(myAWSIoTMQTTClient.publish("/get", humi, 1))
-    if False in res:
-        print('Failed')
-    if False not in res:
-        print(f'Temp in C and F is {c}, {f}.\n Humidity is {humi}%')
-    time.sleep(2)
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+        resubscribe_results = resubscribe_future.result()
+        print("Resubscribe results: {}".format(resubscribe_results))
+
+        for topic, qos in resubscribe_results['topics']:
+            if qos is None:
+                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+
+# Callback when the subscribed topic receives a message
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("Received message from topic '{}': {}".format(topic, payload))
+    global received_count
+    received_count += 1
+    if received_count == count:
+        received_all_event.set()
+
+if __name__ == '__main__':
+    # Spin up resources
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+
+    if useWebsocket == True:
+        credentials_provider = auth.AwsCredentialsProvider.new_default_chain(client_bootstrap)
+        mqtt_connection = mqtt_connection_builder.websockets_with_default_aws_signing(
+            endpoint=endpoint,
+            client_bootstrap=client_bootstrap,
+            region=signing_region,
+            credentials_provider=credentials_provider,
+            http_proxy_options=proxy_options,
+            ca_filepath=rootCAPath,
+            on_connection_interrupted=on_connection_interrupted,
+            on_connection_resumed=on_connection_resumed,
+            client_id=clientId,
+            clean_session=False,
+            keep_alive_secs=30)
+
+    else:
+        mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=endpoint,
+            port=port,
+            cert_filepath=certificatePath,
+            pri_key_filepath=privateKeyPath,
+            client_bootstrap=client_bootstrap,
+            ca_filepath=rootCAPath,
+            on_connection_interrupted=on_connection_interrupted,
+            on_connection_resumed=on_connection_resumed,
+            client_id=clientId,
+            clean_session=False,
+            keep_alive_secs=30,
+            http_proxy_options=proxy_options)
+
+    print("Connecting to {} with client ID '{}'...".format(
+        endpoint, clientId))
+
+    connect_future = mqtt_connection.connect()
+
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+
+    # Subscribe
+    print("Subscribing to topic '{}'...".format(topic))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=topic,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received)
+
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+
+    # Publish message to server desired number of times.
+    # This step is skipped if message is blank.
+    # This step loops forever if count was set to 0.
+    if message:
+        if count == 0:
+            print ("Sending messages until program killed")
+        else:
+            print ("Sending {} message(s)".format(count))
+
+        publish_count = 1
+        while (publish_count <= count) or (count == 0):
+            c, f, h = sensor_data()
+            message = c
+            message = "{} [{}]".format(message, publish_count)
+            print("Publishing message to topic '{}': {}".format(topic, message))
+            message_json = json.dumps(message)
+            mqtt_connection.publish(
+                topic=topic,
+                payload=message_json,
+                qos=mqtt.QoS.AT_LEAST_ONCE)
+            time.sleep(2)
+            publish_count += 1
+
+    # Wait for all messages to be received.
+    # This waits forever if count was set to 0.
+    if count != 0 and not received_all_event.is_set():
+        print("Waiting for all messages to be received...")
+
+    received_all_event.wait()
+    print("{} message(s) received.".format(received_count))
+
+    # Disconnect
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected!")
